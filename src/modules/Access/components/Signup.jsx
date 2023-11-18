38bb42e3b1491d2d53741d89937e0c6c32101f6e
Strayder90 <strayder@live.com>
2023-11-18 16:25:38 +0100

    Removed unused property Image.

--- src/modules/Access/components/Signup.jsx
+++ src/modules/Access/components/Signup.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
-import { Button, Form, Divider, Input, Image } from "semantic-ui-react";
+import { Button, Form, Divider, Input } from "semantic-ui-react";
 
 export default function SignUp() {
   const navigate = useNavigate();
