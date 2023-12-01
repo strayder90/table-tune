c3d831d4761347337e6fea94680627925e9fb1cc
Strayder90 <strayder@live.com>
2023-12-01 23:43:38 +0100

    Enabled service worker.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-// import { registerServiceWorker } from "./serviceWorkerRegistration.js";
+import { registerServiceWorker } from "./serviceWorkerRegistration.js";
 
 import { App } from "./App";
 
@@ -14,4 +14,4 @@ root.render(
   </React.StrictMode>
 );
 
-// registerServiceWorker();
+registerServiceWorker();
