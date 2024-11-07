80e94dbbf132c37d85adb09cb5534a2c0ca9b1a8
strayder90 <strayder@live.com>
2024-11-07 19:22:29 +0100

    Added Semantic Ui library. Added SASS. Added manifest icons and screenshots.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import './sass/style.css';
+import 'semantic-ui-css/semantic.min.css';
 import TableTuneRouter from './TableTuneRouter';
 
 const App = () => {
--- vite.config.js
+++ vite.config.js
@@ -10,12 +10,48 @@ export default defineConfig({
             registerType: 'autoUpdate',
             injectRegister: 'auto',
             manifest: {
-                name: 'table-tune',
+                name: 'Table Tune',
                 short_name: 'TT',
                 description: 'Application to make table reservations smooth experience.',
                 theme_color: '#000000',
+                background_color: '#ffffff',
                 display: 'standalone',
                 icons: [
+                    {
+                        src: 'icon-72x72.png',
+                        type: 'image/png',
+                        sizes: '72x72'
+                    },
+                    {
+                        src: 'icon-128x128.png',
+                        type: 'image/png',
+                        sizes: '128x128'
+                    },
+                    {
+                        src: 'icon-144x144.png',
+                        type: 'image/png',
+                        sizes: '144x144'
+                    },
+                    {
+                        src: 'icon-152x152.png',
+                        type: 'image/png',
+                        sizes: '152x152'
+                    },
+                    {
+                        src: 'icon-192x192.png',
+                        type: 'image/png',
+                        sizes: '192x192'
+                    },
+                    {
+                        src: 'icon-256x256.png',
+                        type: 'image/png',
+                        sizes: '256x256'
+                    },
+                    {
+                        src: 'icon-384x384.png',
+                        type: 'image/png',
+                        sizes: '384x384'
+                    },
                     {
                         src: 'icon-192x192.png',
                         sizes: '192x192',
@@ -25,9 +61,23 @@ export default defineConfig({
                         src: 'icon-512x512.png',
                         sizes: '512x512',
                         type: 'image/png',
-                        purpose: 'any maskable',
                     },
                 ],
+                screenshots: [
+                    {
+                        src: 'screenshot-desktop1280x720.png',
+                        sizes: '1280x720',
+                        type: 'image/png',
+                        purpose: 'any',
+                        form_factor: 'wide'
+                    },
+                    {
+                        src: 'screenshot-mobile750x1334.png',
+                        sizes: '750x1334',
+                        type: 'image/png',
+                        purpose: 'any'
+                    }
+                ]
             },
             workbox: {
                 globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
