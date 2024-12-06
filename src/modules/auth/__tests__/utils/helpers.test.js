d976c84088a171f19330cd30eecd93b0517125c1
strayder90 <strayder@live.com>
2024-12-06 20:12:00 +0100

    Added jest config and dummy test. Also postcss config added and some eslint rules.

---
+++ src/modules/auth/__tests__/utils/helpers.test.js
@@ -0,0 +1,8 @@
+import {sum} from '../../utils/helpers.js';
+
+describe('sum function', () => {
+    test('correctly adds two numbers', () => {
+        const result = sum(2, 3);
+        expect(result).toBe(5);
+    });
+});
