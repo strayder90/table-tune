a94c46c9d31ac0e60d74c55b619b38efcbe4dc51
strayder90 <strayder@live.com>
2024-12-06 22:47:52 +0100

    Added flags to BrowserRouter remove warnings from react router

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ const root = createRoot(container);
 
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
+        <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
             <App/>
         </BrowserRouter>
     </React.StrictMode>
