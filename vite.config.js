984c1708ff2ee0b74a3a5ad0bfe8e5d414df977d
strayder90 <strayder@live.com>
2024-12-06 21:40:06 +0100

    Added vitest and config

--- vite.config.js
+++ vite.config.js
@@ -92,4 +92,11 @@ export default defineConfig({
             },
         }),
     ],
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        coverage: {
+            reporter: ['text', 'html'],
+        },
+    },
 });
