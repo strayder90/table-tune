e726861a2e1168a4ce0fd52f0b3497fe5c807f63
Strayder90 <strayder@live.com>
2025-06-27 23:47:29 +0200

    Error title margin top fixed

--- src/modules/app/components/CustomInput.jsx
+++ src/modules/app/components/CustomInput.jsx
@@ -88,7 +88,6 @@ const CustomInput = forwardRef(({
                                 </div>
                             </Form.Field>
                         );
-
                 }
             }}
         />
