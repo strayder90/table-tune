bf575a579169068b8538edf0cb6367869139d8ea
Strayder90 <strayder@live.com>
2025-06-29 12:31:37 +0200

    Added loader when checking authentication.

--- src/modules/auth/components/AuthGuard.jsx
+++ src/modules/auth/components/AuthGuard.jsx
@@ -1,11 +1,20 @@
 import PropTypes from 'prop-types';
 import {Navigate, useLocation} from 'react-router-dom';
+import {Loader} from 'semantic-ui-react';
 
 const AuthGuard = ({isUserAuthenticated, children}) => {
     const location = useLocation();
 
+    if (isUserAuthenticated === null) {
+        return (
+            <div className='--app_loader'>
+                <Loader active inline='centered' size='large'>Checking authentication...</Loader>
+            </div>
+        );
+    }
+
     if (!isUserAuthenticated) {
-        return <Navigate to='/' replace state={{from: location}}/>;
+        return <Navigate to='/' replace state={{from: location}} />;
     }
 
     return children;
